cac187cd84e6a5975fe9c5cebeb7c866e8c810e2
ChepikPolina <pichepik@gmail.com>
2018-04-17 15:38:44 +0300

    added doxygen.conf

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['demo_2ehpp',['demo.hpp',['../demo_8hpp.html',1,'']]]
-];
--- docs/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getenv_5fvar',['getenv_var',['../demo_8hpp.html#a9026e80c39eec119f1ba071b21e5168a',1,'demo.hpp']]]
-];
